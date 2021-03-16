1a34b780ee1eaf6389b336c90a7855cdf4f53d08
Guillaume Jovelin <jovelinguillaume@gmail.com>
2021-03-16 17:50:03 +0100

    ajout IViews

--- jukebox-spring/src/main/java/m2i/formation/controller/JukeboxController.java
+++ jukebox-spring/src/main/java/m2i/formation/controller/JukeboxController.java
@@ -1,10 +1,17 @@
 package m2i.formation.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.annotation.JsonView;
+
 import m2i.formation.dao.IJukeboxDao;
+import m2i.formation.model.IViews;
+import m2i.formation.model.Jukebox;
 
 @RestController
 @RequestMapping("/api/jukebox")
@@ -12,5 +19,13 @@ public class JukeboxController {
 	
 	@Autowired
 	private IJukeboxDao jukeboxDao;
+	
+	@GetMapping("")
+	@JsonView(IViews.IViewJukebox.class)
+	public List<Jukebox> list() {
+		List<Jukebox> jukeboxes = jukeboxDao.findAll();
+
+		return jukeboxes;
+	}
 
 }
---
+++ jukebox-spring/src/main/java/m2i/formation/model/IViews.java
@@ -0,0 +1,7 @@
+package m2i.formation.model;
+
+public interface IViews {
+	public static interface IViewBasic {}
+	
+	public static interface IViewJukebox extends IViewBasic {}
+}
