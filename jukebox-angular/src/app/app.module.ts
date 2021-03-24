9146b047038d3a178659be878439babeabbf7832
MaximeBara <emouvaxy@gmail.com>
2021-03-24 13:15:36 +0100

    ajout des pages Home/Login & test d'un menu dans la page de login

--- jukebox-angular/src/app/app.module.ts
+++ jukebox-angular/src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import {CommonModule} from '@angular/common';
+import {MenubarModule} from 'primeng/menubar';
+import {InputTextModule} from 'primeng/inputtext';
+import {ButtonModule} from 'primeng/button';
+import {TabViewModule} from 'primeng/tabview';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    LoginPageComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule,
+    CommonModule,
+    MenubarModule,
+    InputTextModule,
+    ButtonModule,
+		TabViewModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
